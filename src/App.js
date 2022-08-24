ceb3f01a431ea125358a55d5f85638f1ec7f4127
Nikhil <nikhilganeshpatil@gmail.com>
2022-08-24 21:58:13 +0530

    I have added the admin portal side of my website.

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import { Container, Navbar, Row, Col } from 'react-bootstrap'
+import AddBook from './components/AddBook'
+import BooksList from './components/BooksList'
+import './index.css'
+import ImageHolder from './components/ImageHolder'
+import Announcements from './components/Announcements'
+
+function App() {
+  const [annId, setAnnId] = useState('')
+
+  const getAnnIdHandler = (id) => {
+    console.log('Id of doc to be edited:', id)
+    setAnnId(id)
+  }
+
+  return (
+    <>
+      <Navbar bg='dark' variant='dark' className='header'>
+        <Container>
+          <Navbar.Brand href='#home'>Library - Firebase CRUD</Navbar.Brand>
+        </Container>
+      </Navbar>
+
+      <Container style={{ width: '400px' }}>
+        <Row>
+          <Col>
+            <AddBook id={annId} setAnnId={setAnnId} />
+          </Col>
+        </Row>
+      </Container>
+      <Container>
+        <Row>
+          <Col>
+            <BooksList getAnnId={getAnnIdHandler} />
+          </Col>
+        </Row>
+      </Container>
+      <Announcements getAnnId={getAnnIdHandler}></Announcements>
+      <ImageHolder></ImageHolder>
+    </>
+  )
+}
+
+export default App
